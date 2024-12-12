4606605aa5878d2fb8c31b216aa82b10e904c2f5
1ltwo <jmz2972924408@163.com>
2024-12-12 16:13:18 +0800

    Changes

--- 1212/perimeter_test.go
+++ 1212/perimeter_test.go
@@ -18,19 +18,23 @@ func TestArea(t *testing.T) {
 	//表格驱动测试在我们要创建一系列相同测试方式的测试用例时很有用
 	//如果你要测试一个接口的不同实现，或者传入函数的数据有很多不同的测试需求，可用此方法。
 	areaTests := []struct {
-		shape Shape
-		want  float64
-	}{
-		{Rectangle{12, 6}, 72},
-		{Circle{10}, 314.1592653589793},
-		{Triangle{12, 6}, 36.0},
+		name    string
+		shape   Shape
+		hasArea float64
+	}{ //断言？
+		{name: "Rectangle", shape: Rectangle{n: 10, m: 10}, hasArea: 100},
+		{name: "Circle", shape: Circle{10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{12, 6}, hasArea: 36},
 	}
 
 	for _, tt := range areaTests {
-		got := tt.shape.Area()
-		if got != tt.want {
-			t.Errorf("got %.2f want %.2f", got, tt.want)
-		}
+		// using tt.name from the case to use it as the `t.Run` test name
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %.2f want %.2f", tt.shape, got, tt.hasArea)
+			}
+		})
 	}
 
 	checkArea := func(t *testing.T, shape Shape, want float64) {
