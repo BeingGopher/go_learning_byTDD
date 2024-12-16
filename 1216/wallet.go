279db1644061dd2a4d441bfee1a62f93eb032bc5
1ltwo <jmz2972924408@163.com>
2024-12-16 15:05:03 +0800

    Changes

--- 1216/wallet.go
+++ 1216/wallet.go
@@ -2,19 +2,33 @@ package main
 
 import "fmt"
 
+type Bitcoin int
+
 type Wallet struct {
 	//在 Go 中，如果一个符号（例如变量、类型、函数等）是以小写符号开头，那么它在 定义它的包之外 就是私有的。
-	balance int
+	balance Bitcoin
 }
 
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	//使用「receiver」变量访问结构体内部的 balance 字段。
-	fmt.Println("address of balance in deposit is", &w.balance)
+	//fmt.Println("address of balance in deposit is", &w.balance)
 	w.balance += amount
 }
 
 // 用 指针 来解决这个问题。指针让我们 指向 某个值，然后修改它。(添加*)
 // 指向 wallet 的指针
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
+}
+
+type Stringer interface {
+	String() string
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	w.balance -= amount
 }
--- 1216/wallet_test.go
+++ 1216/wallet_test.go
@@ -1,25 +1,48 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestWallet(t *testing.T) {
+	//重构
+	assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
+		got := wallet.Balance()
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	}
 
 	wallet := Wallet{}
 
-	wallet.Deposit(10)
+	wallet.Deposit(Bitcoin(10))
 
 	got := wallet.Balance()
 	//通过打印语句调试
-	fmt.Println("address of balance in test is", &wallet.balance)
+	//fmt.Println("address of balance in test is", &wallet.balance)
 
-	want := 10
+	want := Bitcoin(10)
 
 	if got != want {
-		t.Errorf("got %d want %d", got, want)
+		t.Errorf("got %s want %s", got, want)
 	}
 	//目前的状态，会报错。当调用一个函数或方法时，参数会被复制。
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+
+		wallet.Deposit(Bitcoin(10))
+
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+
+		wallet.Withdraw(Bitcoin(10))
+
+		assertBalance(t, wallet, Bitcoin(10))
+	})
 
 }
