a6f5e9eb7f32d2e2b22f83f7b6cfb395e13dc609
1ltwo <jmz2972924408@163.com>
2024-12-17 15:25:42 +0800

    Changes

--- 1217/dictionary.go
+++ 1217/dictionary.go
@@ -1,10 +1,19 @@
 package main
 
+import "errors"
+
 type Dictionary map[string]string
 
-func (d Dictionary) Search(word string) string {
+func (d Dictionary) Search(word string) (string, error) {
 	//接受者、实例类型； 空格后是方法，方法内是传参；最后是返回值类型
-	return d[word]
+	definition, ok := d[word]
+	//map特性，可以返回两个值。第二个值是一个布尔值，表示是否成功找到 key。
+	if !ok {
+		return " ", errors.New("你脑袋怎么尖尖的")
+		//errors.New函数创建一个新的错误实例，错误信息为中文字符串。
+	}
+
+	return definition, nil
 }
 
 func Search(dictionary map[string]string, word string) string {
--- 1217/dictionary_test.go
+++ 1217/dictionary_test.go
@@ -6,10 +6,25 @@ func TestSearch(t *testing.T) {
 	//map 关键字开头，需要两种类型。第一个是键的类型，写在 [] 中。第二个是值的类型，跟在 [] 之后。
 	dictionary := Dictionary{"test": "我不是人，我只是个怪物"}
 	//值的类型可以是任意类型
-	got := dictionary.Search("test")
-	want := "我不是人，我只是个怪物"
 
-	assertStrings(t, got, want)
+	t.Run("known word", func(t *testing.T) {
+		//参数一个是自测试的名称，另一个是包含测试逻辑的匿名函数
+		got, _ := dictionary.Search("test")
+		want := "我不是人，我只是个怪物"
+
+		assertStrings(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		want := "你脑袋怎么尖尖的"
+
+		if err == nil {
+			t.Fatal("expected to get an error")
+		}
+
+		assertStrings(t, err.Error(), want)
+	})
 
 }
 
