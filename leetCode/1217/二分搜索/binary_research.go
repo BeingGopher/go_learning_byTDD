2c9c2b98b082fba0c8e93bad8c0fb87a3ab258a7
1ltwo <jmz2972924408@163.com>
2024-12-17 18:10:46 +0800

    Changes

--- leetCode/1217/二分搜索/binary_research.go
+++ leetCode/1217/二分搜索/binary_research.go
@@ -12,7 +12,8 @@ func search(nums []int, target int) int {
 	// 循环逐步缩小区间范围
 	for left <= right {
 		// 求区间中点
-		mid := left + (right-left)>>1
+		mid := left + (right-left)>>1 //位移操作，在二进制上右移1位，反映到十进制上相当于除以2，加上初始指针位置相当于取到了中间位置的值，相当于直接除。
+		//位移操作代替直接除，性能和速度上有提升。但在处理极大数时，可能会面临溢出的问题。
 
 		// 根据 nums[mid] 和 target 的大小关系
 		// 调整区间范围
