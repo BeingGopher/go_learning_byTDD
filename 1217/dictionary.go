4af9f49159c33e40a0dc0f657c07282051877331
1ltwo <jmz2972924408@163.com>
2024-12-17 14:57:47 +0800

    Changes

--- 1217/dictionary.go
+++ 1217/dictionary.go
@@ -1,5 +1,12 @@
 package main
 
+type Dictionary map[string]string
+
+func (d Dictionary) Search(word string) string {
+	//接受者、实例类型； 空格后是方法，方法内是传参；最后是返回值类型
+	return d[word]
+}
+
 func Search(dictionary map[string]string, word string) string {
 	//函数定义：参数+类型；括号外是返回值类型
 	return dictionary[word]
--- 1217/dictionary_test.go
+++ 1217/dictionary_test.go
@@ -4,17 +4,17 @@ import "testing"
 
 func TestSearch(t *testing.T) {
 	//map 关键字开头，需要两种类型。第一个是键的类型，写在 [] 中。第二个是值的类型，跟在 [] 之后。
-	dictionary := map[string]string{"test": "我不是人，我只是个怪物"}
+	dictionary := Dictionary{"test": "我不是人，我只是个怪物"}
 	//值的类型可以是任意类型
-	got := Search(dictionary, "test")
+	got := dictionary.Search("test")
 	want := "我不是人，我只是个怪物"
 
-	assertString(t, got, want)
+	assertStrings(t, got, want)
 
 }
 
 // 重构
-func assertString(t *testing.T, got, want string) {
+func assertStrings(t *testing.T, got, want string) {
 	//t *testing.T同理，t是参数，空格后是类型
 	t.Helper()
 
