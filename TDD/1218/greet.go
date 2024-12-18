f9c5e6f7a0e8fb898b02df1aed243bdc903a2f0d
1ltwo <jmz2972924408@163.com>
2024-12-18 14:34:32 +0800

    重构

--- TDD/1218/greet.go
+++ TDD/1218/greet.go
@@ -1,11 +1,16 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
+	"io"
+	"os"
 )
 
-func Greet(writer *bytes.Buffer, name string) {
+func Greet(writer io.Writer, name string) {
 	//fmt.Fprintf 和 fmt.Printf 一样，只不过 fmt.Fprintf 会接收一个 Writer 参数，用于把字符串传递过去，而 fmt.Printf 默认是标准输出。
 	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func main() {
+	Greet(os.Stdout, "Elodie")
 }
