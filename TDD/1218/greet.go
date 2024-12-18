ecac80fd33207ca5cb094d8b69998044990f086f
1ltwo <jmz2972924408@163.com>
2024-12-18 14:39:14 +0800

    writer的其他写入途径

--- TDD/1218/greet.go
+++ TDD/1218/greet.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
-	"os"
+	"net/http"
 )
 
 func Greet(writer io.Writer, name string) {
@@ -11,6 +11,11 @@ func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
 }
 
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world") //还可以将数据写入网络
+}
+
+// 当你编写一个 HTTP 处理器（handler）时，你需要给出 http.ResponseWriter 和用于创建请求的 http.Request。在你实现服务器时，你使用 writer 写入了请求。
 func main() {
-	Greet(os.Stdout, "Elodie")
+	http.ListenAndServe(":5000", http.HandlerFunc(MyGreeterHandler))
 }
