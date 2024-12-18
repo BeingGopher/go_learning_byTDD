2b42b1bbde997212fb6183b02983b9a9d694bb72
1ltwo <jmz2972924408@163.com>
2024-12-18 15:06:24 +0800

    重构

--- TDD/1218/Mocking/countdown.go
+++ TDD/1218/Mocking/countdown.go
@@ -1,11 +1,20 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
+	"io"
+	"os"
 )
 
-func Countdown(out *bytes.Buffer) {
+//*bytes.Buffer 可以运行，但最好使用通用接口代替。
+
+func Countdown(out io.Writer) {
 	//使用fmt.Fprint传入一个io.Writer（例如 *bytes.Buffer）并发送一个 string。
 	fmt.Fprint(out, "3")
+}
+
+// 将函数应用到 main中。这样的话，我们就有了一些可工作的软件来确保我们的工作正在取得进展。
+// 在测试的支持下，将功能切分成小的功能点，并使其首尾相连顺利的运行。
+func main() {
+	Countdown(os.Stdout)
 }
