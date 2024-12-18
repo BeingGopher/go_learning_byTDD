9ef010b0089d3e1632dd436fc9092e61ee3209ac
1ltwo <jmz2972924408@163.com>
2024-12-18 16:06:30 +0800

    重构,第三步，实现固定时间输出的功能

--- TDD/1218/Mocking/countdown.go
+++ TDD/1218/Mocking/countdown.go
@@ -4,18 +4,25 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
 //*bytes.Buffer 可以运行，但最好使用通用接口代替。
 
+// 将变量重构为命名变量
+const finalWord = "Go!"
+const countdownStart = 3
+
 func Countdown(out io.Writer) {
 	//使用fmt.Fprint传入一个io.Writer（例如 *bytes.Buffer）并发送一个 string。
 
-	for i := 3; i > 0; i-- {
+	for i := countdownStart; i > 0; i-- {
+		//向下计数的输出1秒的暂停，Go 可以通过 time.Sleep 实现这个功能
+		time.Sleep(1 * time.Second)
 		fmt.Fprintln(out, i)
 	}
-
-	fmt.Fprint(out, "Go!")
+	time.Sleep(1 * time.Second)
+	fmt.Fprint(out, finalWord)
 }
 
 // 将函数应用到 main中。这样的话，我们就有了一些可工作的软件来确保我们的工作正在取得进展。
